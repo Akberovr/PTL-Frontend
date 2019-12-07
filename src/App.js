e63846f5415e1d8009f956c70de5a77b936ce1b1
Akberovr <a.rovshan@code.edu.az>
2019-12-07 01:00:28 +0100

    reactstrap template v.1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,59 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import ListEmployeeComponent from "./component/ListEmployeeComponent";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Component } from 'react';
+import { Table, Button } from 'reactstrap'
+import axios from 'axios';
 
+class App extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      companies : []
+    };
+  }
+  componentDidMount(){
+    axios.get('http://localhost:8080/companies').then((res) => {
+      console.log(res.data.content)
+      this.setState({companies: res.data.content})
+    }); 
+  }
+  render(){
+      const table = this.state.companies.map((a) => {
+       console.log(a.first_name)
+        return(        
+         
+          <tr key = {a.id}>
+            <td>{a.first_name}</td>
+            <td>{a.last_name}</td>
+            <td>{a.email}</td>
+            <td>
+              <Button color="success" size="sm" className="mr-2"> Edit</Button>
+              <Button color="danger" size="sm" className="mr-2"> Delete</Button>
+            </td>
+          </tr>
+ 
+         
 
-function App() {
-  return (  
-    <div>
-      <header>
-
-          <Router>
-              <div className="col-md-6">
-                  <h1 className="text-center" style={style}>React User Application</h1>
-                  <Switch>
-                      <Route path="/" exact component={ListEmployeeComponent} />
-                  </Switch>
-              </div>
-          </Router>
-
-      </header>
-    </div>
-  );
-}
-const style = {
-  color: 'red',
-  margin: '10px'
+        )
+     });
+    return (
+      <div className= "App container">
+        <Table>
+          <thead>
+            <tr>
+            <th>#</th>
+            <th>Id</th>
+            <th>First name</th>
+            <th>Last name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {table}
+          </tbody>
+        </Table>
+        
+      </div>
+    )
+  }
 }
 
 export default App;
